d90a9a7aaa11d9bd16edd2be71b01c1dd78819e5
David Francois Droz <david.droz@outlook.com>
2017-06-07 16:36:11 +0200

    Bug fix. Added graph labels

--- makeHistograms.py
+++ makeHistograms.py
@@ -14,6 +14,39 @@ import matplotlib.pyplot as plt
 import sys
 from matplotlib.backends.backend_pdf import PdfPages
 
+def getLabels():
+	
+	lab = []
+	
+	ebgo = 'BGO_E_layer_'
+	for i in range(14):
+		lab.append(ebgo + str(i))
+	erms = 'BGO_E_RMS_layer_'
+	for i in range(14):
+		lab.append(erms + str(i))
+	lab.append('BGO_RMS_longitudinal')
+	lab.append('BGO_RMS_radial')
+	lab.append('BGO_E_total_corrected')
+	lab.append('BGO_total_hits')
+	
+	for i in range(2):
+		lab.append('PSD_E_layer_' + str(i))
+	for i in range(2):
+		lab.append('PSD_hits_layer_' + str(i))
+	for k in ['1a','1b','2a','2b']:
+		lab.append('PSD_E_RMS_layer_' + k)
+		
+	lab.append('STK_NClusters')
+	lab.append('STK_NTracks')
+	for i in range(8):
+		lab.append('STK_E_' + str(i))
+	for i in range(8):
+		lab.append('STK_E_RMS_' + str(i))
+		
+	lab.append('timestamp')
+	lab.append('label')
+	
+	return lab
 
 def _normalise(arr):
 	for i in xrange(arr.shape[1]):
@@ -28,17 +61,19 @@ if __name__ == '__main__':
 	arr_elecs = _normalise(np.load(sys.argv[1])[:,0:-2])
 	arr_prots = _normalise(np.load(sys.argv[2])[:,0:-2])
 	
+	lab = getLabels()
+	
 	pp = PdfPages('multipage.pdf')
 	
 	for i in xrange(arr_elecs.shape[1]):
 		
+		f1 = plt.figure()
 		plt.hist(arr_elecs[:,i], 50, normed=1,histtype='step', label='e')
 		plt.hist(arr_prots[:,i], 50, normed=1,histtype='step', label='p')
 		plt.legend(loc='best')
 		#~ plt.xscale("log")
 		plt.yscale('log')
-		plt.title('var '+str(i))
+		plt.title(lab[i])
 		plt.savefig(pp, format='pdf')
-		
-	
+		plt.close(f1)
 	pp.close()
