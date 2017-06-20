f573d96d4b2e084cd7815fa70cb1dc68211ec6e9
David Francois Droz <david.droz@outlook.com>
2017-06-20 13:41:21 +0200

    Initial commit

---
+++ PCA/PCA.py
@@ -0,0 +1,69 @@
+'''
+
+PCA.py
+
+PCA decomposition of DAMPE data
+
+
+
+'''
+
+from __future__ import print_function, division, absolute_import
+
+import numpy as np
+import time
+import pickle
+import sys
+import os
+import random
+import hashlib
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+from sklearn.decomposition import PCA
+from sklearn.metrics import roc_curve, roc_auc_score, precision_score, average_precision_score, precision_recall_curve, recall_score
+from sklearn.metrics import f1_score
+from sklearn.model_selection import RandomizedSearchCV
+
+def XY_split(fname):
+	arr = np.load(fname)
+	X = arr[:,0:-2]				# Last two columns are timestamp and particle ID
+	Y = arr[:,-1]
+	return X,Y
+def load_training(fname='../dataset_train.npy'): return XY_split(fname)
+def load_validation(fname='../dataset_validate.npy'): return XY_split(fname)
+def load_test(fname='../dataset_test.npy'): return XY_split(fname)
+
+
+def _run():
+	
+
+	t0 = time.time()
+	
+	X_train, Y_train = load_training()
+	
+	p = PCA(n_components=5)
+	p.fit(X_train)
+	
+	electrons = p.transform( np.load('/home/drozd/analysis/fraction1/data_test_elecs_1.npy')[:,0:-2]  )
+	protons = p.transform( np.load('/home/drozd/analysis/fraction1/data_test_prots_1.npy')[:,0:-2]  )
+	
+	for i in range(5):
+		fig1 = plt.figure()
+		plt.hist(electrons[:,i],50,histtype='step',label='e')
+		plt.hist(protons[:,i],50,histtype='step',label='p')
+		plt.legend(loc='best')
+		plt.title('PCA - PC component ' + str(i))
+		plt.savefig('pc'+str(i))
+	
+		
+		
+	
+if __name__ == '__main__' :
+	
+	_run()
+
+
+	
