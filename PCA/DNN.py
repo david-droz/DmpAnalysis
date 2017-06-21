ddc9c76d699a7745b5a3965ebb654117db808ca8
David Francois Droz <david.droz@outlook.com>
2017-06-21 13:44:54 +0200

    Initial commit

---
+++ PCA/DNN.py
@@ -0,0 +1,106 @@
+from __future__ import print_function, division, absolute_import
+
+import numpy as np
+import time
+import pickle
+import sys
+import os
+import random
+import hashlib
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+from sklearn.decomposition import PCA
+from sklearn.metrics import roc_curve, roc_auc_score, precision_score, average_precision_score, precision_recall_curve, recall_score
+from sklearn.metrics import f1_score
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.preprocessing import StandardScaler
+
+from mpl_toolkits.mplot3d import Axes3D
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout
+from keras.callbacks import ModelCheckpoint, EarlyStopping, Callback, LearningRateScheduler, ReduceLROnPlateau
+from keras.constraints import maxnorm
+from keras.layers.advanced_activations import PReLU, ELU, LeakyReLU
+
+def XY_split(fname):
+	arr = np.load(fname)
+	X = arr[:,0:-2]				# Last two columns are timestamp and particle ID
+	Y = arr[:,-1]
+	return X,Y
+
+def run(applyPCA):
+	
+	t0 = time.time()
+	
+	balanced = True
+	
+	if balanced:	
+		X_train, Y_train = XY_split('/home/drozd/analysis/fraction1/dataset_train.npy')
+		X_val, Y_val = XY_split('/home/drozd/analysis/fraction1/dataset_validate_1.npy')	
+	else:
+		X_train, Y_train = XY_split('/home/drozd/analysis/dataset_train.npy')
+		X_val, Y_val = XY_split('/home/drozd/analysis/dataset_validate.npy')		
+	
+	X_train = StandardScaler().fit_transform(X_train)
+	X_val = StandardScaler().fit_transform(X_val)
+	
+	if applyPCA:
+		p = PCA(n_components=40)		# 40 components explain ~97.8% of variance
+		p.fit(X_train)
+		X_train = p.transform(X_train)
+		X_val = p.transform(X_val)
+		print('----- PCA -----')
+	else:
+		print('----- no PCA -----')
+	
+	model = Sequential()
+	model.add(Dense(40,
+					input_shape=(X_train.shape[1],),
+					kernel_initializer='uniform',
+					activation='relu'))
+	model.add(Dense(20,kernel_initializer='uniform',activation='relu'))
+	model.add(Dense(10,kernel_initializer='uniform',activation='relu'))
+	model.add(Dense(1,kernel_initializer='uniform',activation='sigmoid'))
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['binary_accuracy'])
+
+	callbacks = []
+	
+	history = model.fit(X_train,Y_train,batch_size=100,epochs=50,verbose=2,callbacks=callbacks,validation_data=(X_val,Y_val))
+	
+	predictions_binary = np.around(model.predict(X_val))		# Array of 0 and 1
+	predictions_proba = model.predict_proba(X_val)				# Array of numbers [0,1]
+	
+	purity = precision_score(Y_val,predictions_binary)			# Precision:  true positive / (true + false positive). Purity (how many good events in my prediction?)
+	completeness = recall_score(Y_val,predictions_binary)		# Recall: true positive / (true positive + false negative). Completeness (how many good events did I find?)
+	F1score = f1_score(Y_val,predictions_binary)				# Average of precision and recall
+	
+	l_precision, l_recall, l_thresholds = precision_recall_curve(Y_val,predictions_proba)
+	
+
+	# Precision and Recall that maximise F1 score
+	l_f1 = []
+	for i in range(len(l_precision)):
+		l_f1.append( 2*(l_precision[i] * l_recall[i])/(l_precision[i] + l_recall[i])    )
+	mf1 = max(l_f1)
+	for i in range(len(l_f1)):
+		if l_f1[i] == mf1:
+			prec_95 = l_precision[i]
+			recall_95 = l_recall[i]
+					
+	print("Precision:", prec_95)
+	print("Recall:", recall_95)
+	print("Max F1:", mf1)
+	
+	del X_train, X_val, Y_train, Y_val, history, predictions_binary, predictions_proba
+	
+		
+		
+	
+if __name__ == '__main__' :
+	
+	run(True)
+	run(False)
