ff28fd514eb26a6a866ca5273862225bf2409c17
David Francois Droz <david.droz@outlook.com>
2017-07-06 16:25:38 +0200

    Rework: now training happens on a per-bin basis

--- DNN/predHistoEnergyDependent.py
+++ DNN/predHistoEnergyDependent.py
@@ -7,11 +7,8 @@ Train a model, and then plot the histogram of classifier score on several energy
 from __future__ import division, print_function, absolute_import
 
 import numpy as np
-import time
-import pickle
 import sys
 import os
-import random
 
 import matplotlib
 matplotlib.use('Agg')
@@ -20,8 +17,6 @@ import matplotlib.pyplot as plt
 from keras.models import Sequential, load_model
 from keras.layers.core import Dense, Dropout
 from keras.callbacks import ModelCheckpoint, EarlyStopping, Callback, LearningRateScheduler, ReduceLROnPlateau
-from keras.constraints import maxnorm
-from keras.layers.advanced_activations import PReLU, ELU, LeakyReLU
 
 from sklearn.metrics import roc_curve, roc_auc_score, precision_score, average_precision_score, precision_recall_curve, recall_score
 from sklearn.metrics import f1_score
@@ -41,9 +36,12 @@ def getClassifierScore(truth,pred):
 			
 	return elecs, prots
 
-def getParticleSet(fname):
+def getParticleSet(fname, Emin, Emax):
 	arr = np.load(fname)
-	X = arr[:,0:-2]				### Last two columns are timestamp and particle id
+	arr = arr[ arr[:,30] > Emin ]
+	arr = arr[ arr[:,30] < Emax ]
+	
+	X = arr[:,0:-2]	
 	Y = arr[:,-1]
 	X = _normalise(X)
 	r = np.concatenate(( X, Y.reshape(( Y.shape[0], 1 )) ) , axis=1)
@@ -51,20 +49,21 @@ def getParticleSet(fname):
 	return r
 
 	
-def run():
+def run(Emin, Emax, BDT=False):
 	
-	train_e = getParticleSet('/home/drozd/analysis/data_train_elecs.npy')
-	train_p = getParticleSet('/home/drozd/analysis/data_train_prots.npy')
+	figureName = 'pred_'+str(int(Emin/1000))+'-'+str(int(Emax/1000))
+	if os.path.isfile(figureName+'.png'): return
+	
+	train_e = getParticleSet('/home/drozd/analysis/data_train_elecs.npy', Emin, Emax)
+	train_p = getParticleSet('/home/drozd/analysis/data_train_prots.npy', Emin, Emax)
 	train = np.concatenate(( train_e, train_p ))
 	np.random.shuffle(train)
-	
 	X_train = train[:,0:-1]
 	Y_train = train[:,-1]
 	del train_e,train_p, train
 
-	val_e = np.concatenate((getParticleSet('/home/drozd/analysis/fraction1/data_validate_elecs_1.npy') , getParticleSet('/home/drozd/analysis/fraction1/data_test_elecs_1.npy') ))
-	val_p = np.concatenate((getParticleSet('/home/drozd/analysis/fraction1/data_validate_prots_1.npy') , getParticleSet('/home/drozd/analysis/fraction1/data_test_prots_1.npy') ))
-	
+	val_e = getParticleSet('/home/drozd/analysis/NoBootStrap/data_validate_elecs_under_100.npy', Emin, Emax)
+	val_p = getParticleSet('/home/drozd/analysis/NoBootStrap/data_validate_prots_under_100.npy', Emin, Emax)
 	val = np.concatenate(( val_e, val_p ))
 	np.random.shuffle(val)
 	
@@ -73,9 +72,12 @@ def run():
 	
 	del val_e, val_p, val
 	
-	modelName = 'myModel.model'
-	
-	if not os.path.isfile(modelName):
+	if BDT:
+		model = GradientBoostingClassifier(n_estimators=100, learning_rate=0.1,max_depth=3, min_samples_leaf=0.0001)
+		model.fit(X_train, Y_train)
+		
+		predictions = model.predict_proba(X_val)[:,1]
+	else:
 		model = Sequential()
 		model.add(Dense(300,input_shape=(X_train.shape[1],),kernel_initializer='he_uniform',activation='relu'))
 		model.add(Dropout(0.1))
@@ -89,72 +91,36 @@ def run():
 		rdlronplt = ReduceLROnPlateau(monitor='loss',patience=3,min_lr=0.001)
 		earl = EarlyStopping(monitor='loss',min_delta=0.0001,patience=5)
 		callbacks = [rdlronplt,earl]
-		
-		history = model.fit(X_train,Y_train,batch_size=150,epochs=40,verbose=0,callbacks=callbacks,validation_data=(X_val,Y_val))
-		model.save(modelName)
-	else:
-		model = load_model(modelName)
-	
-	
-	
-	# --------------------------------
-	
-	
-	
-	
-	
-	for i in range(bin_edges.shape[0]-1):
-		
-		graphName = 'predHisto_'+str(int(bin_edges[i]))+'-'+str(int(bin_edges[i+1]))
-		if os.path.isfile(graphName+'.png'):
-			continue
-		
-		arr_e = np.concatenate((np.load('/home/drozd/analysis/fraction1/data_validate_elecs_1.npy'),np.load('/home/drozd/analysis/fraction1/data_test_elecs_1.npy') ))
-		
-		arr_e = arr_e[ arr_e[:,30] > bin_edges[i] ] 
-		arr_e = arr_e[ arr_e[:,30] < bin_edges[i+1] ]
-		X = arr_e[:,0:-2]
-		Y = arr_e[:,-1]
-		X = _normalise(X)
-		arr_e = np.concatenate(( X, Y.reshape(( Y.shape[0], 1 )) ) , axis=1)
-		del X, Y
-		
-		arr_p = np.concatenate((np.load('/home/drozd/analysis/fraction1/data_validate_prots_1.npy'),np.load('/home/drozd/analysis/fraction1/data_test_prots_1.npy') ))
-		arr_p = arr_p[ arr_p[:,30] > bin_edges[i] ] 
-		arr_p = arr_p[ arr_p[:,30] < bin_edges[i+1] ]
-		X = arr_p[:,0:-2]
-		Y = arr_p[:,-1]
-		X = _normalise(X)
-		arr_p = np.concatenate(( X, Y.reshape(( Y.shape[0], 1 )) ) , axis=1)
-		del X, Y
-		
-		bigarr = np.concatenate(( arr_e, arr_p ))
-		del arr_e, arr_p
-		np.random.shuffle(bigarr)
-		X_val = bigarr[:,0:-1]
-		Y_val = bigarr[:,-1]
-		del bigarr
+		history = model.fit(X_train,Y_train,batch_size=150,epochs=50,verbose=0,callbacks=callbacks,validation_data=(X_val,Y_val))
 		
 		predictions = model.predict(X_val)
-		elecs_p, prots_p = getClassifierScore(Y_val,predictions)	
-		
-		Nbins = 50
-		binList = [x/Nbins for x in range(0,Nbins+1)]		
-		fig = plt.figure()
-		plt.hist(elecs_p,bins=binList,label='e',alpha=0.7,histtype='step',color='green')
-		plt.hist(prots_p,bins=binList,label='p',alpha=0.7,histtype='step',color='red')
-		plt.xlabel('Classifier score')
-		plt.ylabel('Number of events')
-		plt.title(str(int(bin_edges[i]))+' - '+str(int(bin_edges[i+1])))
-		plt.legend(loc='upper center')
-		plt.yscale('log')
-		plt.savefig(graphName)
-		plt.close(fig)
+
+	
+	elecs_p, prots_p = getClassifierScore(Y_val,predictions)	
+	
+	Nbins = 50
+	binList = [x/Nbins for x in range(0,Nbins+1)]		
+	fig = plt.figure()
+	plt.hist(elecs_p,bins=binList,label='e',alpha=0.7,histtype='step',color='green')
+	plt.hist(prots_p,bins=binList,label='p',alpha=0.7,histtype='step',color='red')
+	plt.xlabel('Classifier score')
+	plt.ylabel('Number of events')
+	plt.title( str(int(Emin/1000))+' GeV - '+str(int(Emax/1000))+' GeV' )
+	plt.legend(loc='upper center')
+	plt.yscale('log')
+	plt.savefig(figureName)
+	plt.close(fig)
+	
 	
 	
 if __name__ == '__main__' :
 	
-	Nlogbins = np.logspace(4,7,4) 
+	Nbins = 4
+	logbins = np.logspace(5,7,Nbins+1)
 	
-	run()
+	if len(sys.argv[1]) > 1: BDT = True
+	else: BDT = False
+	
+	for i in range(Nbins):
+		run(logbins[i],logbins[i+1],BDT)
 
