9941ad9cf8cff95b2244c7f8c446496ceb2ff24e
David Francois Droz <david.droz@outlook.com>
2017-07-13 11:22:21 +0200

    Various modifications, trial and error, does not work

--- DNN/predHistoEnergyDependent.py
+++ DNN/predHistoEnergyDependent.py
@@ -37,14 +37,17 @@ def getClassifierScore(truth,pred):
 			
 	return elecs, prots
 
-def getParticleSet(fname, Emin, Emax):
+def getParticleSet(fname, Emin, Emax, BDT=False):
 	arr = np.load(fname)
-	arr = arr[ arr[:,30] > Emin ]
-	arr = arr[ arr[:,30] < Emax ]
+	
+	if Emin > 0:
+		arr = arr[ arr[:,30] > Emin ]
+		arr = arr[ arr[:,30] < Emax ]
 	
 	X = arr[:,0:-2]	
 	Y = arr[:,-1]
-	X = _normalise(X)
+	if not BDT:
+		X = _normalise(X)
 	r = np.concatenate(( X, Y.reshape(( Y.shape[0], 1 )) ) , axis=1)
 	del arr, X, Y
 	return r
@@ -59,16 +62,26 @@ def run(Emin, Emax, BDT=False):
 		
 	if os.path.isfile(figureName+'.png'): return
 	
-	train_e = getParticleSet('/home/drozd/analysis/fraction1/data_train_elecs.npy', Emin, Emax)
-	train_p = getParticleSet('/home/drozd/analysis/fraction1/data_train_prots.npy', Emin, Emax)
+	train_e = getParticleSet('/home/drozd/analysis/fraction1/data_train_elecs.npy', -1, Emax, BDT)
+	train_p = getParticleSet('/home/drozd/analysis/fraction1/data_train_prots.npy', -1, Emax, BDT)
 	train = np.concatenate(( train_e, train_p ))
 	np.random.shuffle(train)
 	X_train = train[:,0:-1]
 	Y_train = train[:,-1]
+	
+	ghost = plt.figure()
+	plt.hist(train_e[:,30],50,histtype='step',label='e',color='green')
+	plt.hist(train_p[:,30],50,histtype='step',label='p',color='red')
+	plt.title(str(int(Emin/1000))+' - '+str(int(Emax/1000)))
+	plt.xlabel('Binned normalised energy')
+	plt.legend(loc='best')
+	plt.savefig(figureName.replace('pred_','spectre_'))
+	plt.close(ghost)
+	
 	del train_e,train_p, train
 
-	val_e = getParticleSet('/home/drozd/analysis/fraction1/data_validate_elecs_1.npy', Emin, Emax)
-	val_p = getParticleSet('/home/drozd/analysis/fraction1/data_validate_prots_1.npy', Emin, Emax)
+	val_e = getParticleSet('/home/drozd/analysis/fraction1/data_validate_elecs_1.npy', Emin, Emax, BDT)
+	val_p = getParticleSet('/home/drozd/analysis/fraction1/data_validate_prots_1.npy', Emin, Emax, BDT)
 	val = np.concatenate(( val_e, val_p ))
 	np.random.shuffle(val)
 	
@@ -119,6 +132,83 @@ def run(Emin, Emax, BDT=False):
 	plt.close(fig)
 	
 	
+	arr_e = np.load('/home/drozd/analysis/fraction1/data_test_elecs_1.npy')
+	X_e = _normalise(arr_e[:,0:-2])
+	Y_e = arr_e[:,-1]
+	arr_e = np.concatenate(( X_e, Y_e.reshape(( Y_e.shape[0], 1 )) ) , axis=1)
+	del X_e, Y_e
+	arr_p = np.load('/home/drozd/analysis/fraction1/data_test_prots_1.npy')
+	X_p = _normalise(arr_p[:,0:-2])
+	Y_p = arr_p[:,-1]
+	arr_p = np.concatenate(( X_p, Y_p.reshape(( Y_p.shape[0], 1 )) ) , axis=1)
+	del X_p, Y_p
+	tst = np.concatenate(( arr_e, arr_p ))
+	np.random.shuffle(tst)
+	del arr_e,arr_p
+	X_tst = tst[:,0:-1]
+	Y_tst = tst[:,-1]
+	del tst
+	if BDT:
+		pred_tst = model.predict_proba(X_tst)[:,1]
+	else:
+		pred_tst = model.predict(X_tst)
+	del X_tst
+	elecs_pd, prots_pd = getClassifierScore(Y_tst,pred_tst)
+	fig = plt.figure()
+	plt.hist(elecs_pd,bins=binList,label='e',alpha=0.7,histtype='step',color='green')
+	plt.hist(prots_pd,bins=binList,label='p',alpha=0.7,histtype='step',color='red')
+	plt.xlabel('Classifier score')
+	plt.ylabel('Number of events')
+	plt.title( 'Full validation range, limited train range \n'+str(int(Emin/1000))+' GeV - '+str(int(Emax/1000))+' GeV' )
+	plt.legend(loc='upper center')
+	plt.yscale('log')
+	plt.savefig(figureName.replace('pred','full'))
+	plt.close(fig)
+	
+
+	
+def plotFullSpectrum():
+	arr_e_t = np.load('/home/drozd/analysis/fraction1/data_train_elecs.npy')[:,30]
+	arr_p_t = np.load('/home/drozd/analysis/fraction1/data_train_prots.npy')[:,30]
+	arr_e_v = np.load('/home/drozd/analysis/fraction1/data_validate_elecs_1.npy')[:,30]
+	arr_p_v = np.load('/home/drozd/analysis/fraction1/data_validate_prots_1.npy')[:,30]
+	
+	spectre1 = plt.figure()
+	plt.hist(np.log10(arr_e_t),50,histtype='step',color='green',label='e')
+	plt.hist(np.log10(arr_p_t),50,histtype='step',color='red',label='p')
+	plt.yscale('log')
+	plt.xlabel('log10(Energy [MeV])')
+	plt.ylabel('Nr of events')
+	plt.title('train')
+	plt.legend(loc='best')
+	plt.savefig('spectrum-train')
+	
+	spectre2 = plt.figure()
+	plt.hist(np.log10(arr_e_v),50,histtype='step',color='green',label='e')
+	plt.hist(np.log10(arr_p_v),50,histtype='step',color='red',label='p')
+	plt.yscale('log')
+	plt.xlabel('log10(Energy [MeV])')
+	plt.ylabel('Nr of events')
+	plt.title('validate')
+	plt.legend(loc='best')
+	plt.savefig('spectrum-validate')
+	
+	del arr_e_t, arr_p_t, arr_e_v, arr_p_v
+	
+	arr_e = np.load('/home/drozd/analysis/fraction1/data_validate_elecs_1.npy')[:,0:-2]	
+	arr_e = _normalise(arr_e)
+	arr_p = np.load('/home/drozd/analysis/fraction1/data_validate_prots_1.npy')[:,0:-2]	
+	arr_p = _normalise(arr_p)
+	
+	spectre3 = plt.figure()
+	plt.hist(arr_e[:,30],50,histtype='step',color='green',label='e')
+	plt.hist(arr_p[:,30],50,histtype='step',color='red',label='p')
+	plt.yscale('log')
+	plt.xlabel('Normalised energy')
+	plt.ylabel('Nr of events')
+	plt.legend(loc='best')
+	plt.title('normalised')
+	plt.savefig('spectrum-normed')
 	
 if __name__ == '__main__' :
 	
@@ -131,3 +221,4 @@ if __name__ == '__main__' :
 	for i in range(Nbins):
 		run(logbins[i],logbins[i+1],BDT)
 
+	plotFullSpectrum()
