1833e4ca08f3f129ff924241e2de74c43887208a
zimmer <zimmer@fysik.su.se>
2017-07-18 17:20:38 +0200

    fixes

--- TMVA/applyClassifier.py
+++ TMVA/applyClassifier.py
@@ -10,12 +10,6 @@ from shutil import copy
 from array import array
 from argparse import ArgumentParser
 
-def init():
-	from ROOT import TFile, TTree, gSystem, TObject, gROOT
-	gSystem.Load("libDmpEvent.so")
-	gROOT.SetBatch(True)
-	from ROOT import DmpChain
-
 def main(args=None):
 	usage = "Usage: %(prog)s [options]"
 	description = "adding MVAtree to existing 2A file, will create a copy"
@@ -23,10 +17,10 @@ def main(args=None):
 	parser.add_argument("-i","--infile",dest='infile',type=str,default=None, help='name of input file',required=True)
 	parser.add_argument("-o","--outfile",dest='outfile',type=str,default=None, help='name of output file',required=True)
 	opts = parser.parse_args(args)
-	try:
-		init()
-	except Exception as err:
-		raise RuntimeError(err)
+	from ROOT import TFile, TTree, gSystem, TObject, gROOT
+	gSystem.Load("libDmpEvent.so")
+	gROOT.SetBatch(True)
+	from ROOT import DmpChain
 	# first, make copy of outfile
 	copy(opts.infile,opts.outfile)
 	# next, load CollectionTree
@@ -45,7 +39,7 @@ def main(args=None):
 	# next is the usual event loop
 	nevts = dpch.GetEntries()
 	for i in xrange(nevts):
-		pev = dpch.DmpEvent(i)
+		pev = dpch.GetDmpEvent(i)
 		# here you can add the usual logic, just *never* use continue
 		
 		# finally, compute scores in the end.
