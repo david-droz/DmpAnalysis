30540cf13d2a3eaaede5441c020514d8ca836e3c
David Francois Droz <david.droz@outlook.com>
2017-07-18 11:15:35 +0200

    Improvement and bug fixes

--- selection/runSelection.sh
+++ selection/runSelection.sh
@@ -4,13 +4,23 @@ INFILE=
 particle=
 
 ###
-cd /dampe/data3/users/ddroz/analysis/selection_v3/
+cd /dampe/data3/users/ddroz/analysis/selection_v4/
 
 source /cvmfs/dampe.cern.ch/rhel6-64/etc/setup.sh
 dampe_init
 
+NLINES=$(cat $INFILE | wc -l)
+
+PREFIX=${INFILE/".txt"/"-part"}
+DIRNAME=${INFILE/".txt"/""}
+
+split -l 10 -d $INFILE $PREFIX
+
+mkdir $DIRNAME
+mv ${PREFIX}* $DIRNAME
+
 i=0
-for f in $(cat $INFILE)
+for f in $(ls ${DIRNAME}/*)
 do
 	python /dampe/data3/users/ddroz/analysis/DmpAnalysis/selection/selection.py ${f} ${particle} ${i}
 	((i+=1))
