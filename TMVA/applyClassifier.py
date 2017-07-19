81bfd65ccfb83924a6479354bbc15ac2c5c3281a
David Francois Droz <david.droz@outlook.com>
2017-07-19 17:23:10 +0200

    W.I.P.

--- TMVA/applyClassifier.py
+++ TMVA/applyClassifier.py
@@ -10,13 +10,19 @@ from shutil import copy
 from numpy import zeros
 from argparse import ArgumentParser
 
+from keras.models import load_model
+from sklearn.externals import joblib
+
 def main(args=None):
 	usage = "Usage: %(prog)s [options]"
 	description = "adding MVAtree to existing 2A file, will create a copy"
 	parser = ArgumentParser(usage=usage, description=description)
 	parser.add_argument("-i","--infile",dest='infile',type=str,default=None, help='name of input file',required=True)
 	parser.add_argument("-o","--outfile",dest='outfile',type=str,default=None, help='name of output file',required=True)
+	parser.add_argument("-m","--model",dest="model",type=str,default="model.model",help="name of Keras model file")
+	parser.add_argument("-b","--bdt",dest="bdt",type=str,default="bdt.pick",help="name of sklearn BDT model file")
 	opts = parser.parse_args(args)
+	
 	from ROOT import TFile, TTree, gSystem, TObject, gROOT
 	gSystem.Load("libDmpEvent.so")
 	gROOT.SetBatch(True)
@@ -51,6 +57,12 @@ def main(args=None):
 	# here comes some keras 'magic'
 	# ...
 	# i'm assuming you compute scores as DNN_sk & BDT_sk
+	
+	DNN = load_model(args.model)
+	DNN_sk = DNN.predict(X_norm)
+	
+	BDT = joblib.load(args.bdt)
+	BDT_sk = BDT.predict_proba(X)[:,1]
 	
 	# now loop again, creating scoring variables
 	print 'store scores'
