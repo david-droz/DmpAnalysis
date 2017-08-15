d001a3ae6e1b1d65d427253b86948e48a6833433
David Francois Droz <david.droz@outlook.com>
2017-08-15 11:32:51 +0200

    Initial commit, test in progress

---
+++ histoRedux.py
@@ -0,0 +1,56 @@
+'''
+
+Compares the histograms of all variables for the machine learning, for electrons and protons
+Substracts the two histograms, then computes the area of the result
+
+'''
+
+from __future__ import print_function, division, absolute_import
+
+
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+
+
+def run():
+	val_e = np.load('/home/drozd/analysis/newData/data_validate_elecs_under_1.npy') 
+	val_p = np.load('/home/drozd/analysis/newData/data_validate_prots_under_1.npy')[0:val_e.shape[0],:]
+	
+	nrofvars = val_e.shape[1] - 2
+	
+	for i in range(nrofvars):
+		val_e[:,i] = val_e[:,i] / val_e[:,i].max(axis=0)
+		val_p[:,i] = val_p[:,i] / val_p[:,i].max(axis=0)
+	
+	binList = [x/100. for x in range(0,101)]
+	
+	if not os.path.isdir('images'): os.mkdir('images')
+	
+	for n in range(nrofvars):
+		
+		fig1 = plt.figure()
+		e_n, e_bins, e_patches = plt.hist(val_e[:,n],bins=binList,histtype='step',color='green',label='e')
+		p_n, p_bins, p_patches = plt.hist(val_p[:,n],bins=binList,histtype='step',color='red',label='p')
+		
+		e_bins_c = [(e_bins[i]+e_bins[i+1])/2. for i in range(len(e_n))]
+		p_bins_c = [(p_bins[i]+p_bins[i+1])/2. for i in range(len(p_n))]
+		
+		sub = [abs(e_n[i] - p_n[i]) for i in range(len(e_n))]
+		
+		
+		plt.hist(sub,bins=binList,histtype='step',color='blue',label='sub')
+		plt.legend(loc='best')
+		
+		plt.savefig('images/'+str(n))
+	
+	
+	
+	
+	
+	
+if __name__ == '__main__':
+	
+	run()
