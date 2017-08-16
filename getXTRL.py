c70bcb8ec5535c76cccc7ffcb3efce154fee990b
David Francois Droz <david.droz@outlook.com>
2017-08-16 18:52:08 +0200

    Added various graphs and changes

--- getXTRL.py
+++ getXTRL.py
@@ -15,7 +15,7 @@ import time
 from ROOT import gSystem
 gSystem.Load("libDmpEvent.so")
 import ROOT
-
+import pickle
 
 
 def computeSumRMS(pev):
@@ -23,13 +23,15 @@ def computeSumRMS(pev):
 	RMSarr = pev.pEvtBgoRec().GetRMS2()
 	s = 0
 	for i in xrange(14): s+=RMSarr[i]
+	#~ 
+	#~ s = pev.pEvtBgoRec().GetRMS_r()
 	
 	return s
 	
 	
 def computeFLAST(pev):
 	
-	return pev.pEvtBgoRec().GetELayer(13) / pev.pEvtBgoRec().GetElectronEcor()
+	return pev.pEvtBgoRec().GetELayer(13) / pev.pEvtBgoRec().GetTotalEnergy()
 	
 	
 def buildXTRL(pev):
@@ -37,9 +39,17 @@ def buildXTRL(pev):
 	Flast = computeFLAST(pev)
 	SumRMS = computeSumRMS(pev)
 	
-	XTRL = Flast * SumRMS / 8e+6
+	XTRL = Flast * (SumRMS**4) / 8e+6
 	
-	return XTRL
+	return XTRL, Flast, SumRMS
+	
+def cuts(pev):
+	
+	if pev.pEvtBgoRec().GetTotalEnergy() < 1e+3 : return False
+	if pev.pEvtBgoRec().GetELayer(13) == 0 : return False
+	if computeFLAST(pev) > 0.2 : return False
+	
+	return True
 	
 	
 def run():
@@ -67,17 +77,33 @@ def run():
 	
 	xtrl_e = []
 	xtrl_p = []
+	flast_e = []
+	flast_p = []
+	rms_e = []
+	rms_p = []
 	for ne in xrange(nrofe):
 		pev = chain_e.GetDmpEvent(ne)
-		xtrl_e.append( buildXTRL(pev) )
+		
+		if not cuts(pev): continue
+		x,f,r = buildXTRL(pev)
+		xtrl_e.append( x )
+		flast_e.append( f )
+		rms_e.append( r )
+		
 	for np in xrange(nrofp):
 		ppev = chain_p.GetDmpEvent(np)
-		xtrl_p.append( buildXTRL(ppev) )
+		
+		if not cuts(pev): continue
+			
+		x,f,r = buildXTRL(pev)
+		xtrl_p.append( x )
+		flast_p.append( f )
+		rms_p.append( r )
 		
 	with open("XTRL.pick",'w') as h:
 		pickle.dump([xtrl_e,xtrl_p],h)
 		
-	return xtrl_e , xtrl_p
+	return xtrl_e , xtrl_p, flast_e, flast_p, rms_e, rms_p
 	
 def makeHisto(a,b):
 	import matplotlib
@@ -96,12 +122,51 @@ def makeHisto(a,b):
 	plt.yscale('log')
 	plt.xlabel('XTRL')
 	plt.grid(True)
+	plt.legend(loc='best')
 	plt.savefig('XTRL')
 	
+def make2DHisto(a,b,c,d):
+	
+	import matplotlib
+	matplotlib.use('Agg')
+	import matplotlib.pyplot as plt
+	
+	fig1 = plt.figure()
+	plt.hist2d(c+d,a+b,bins=80,norm=matplotlib.colors.LogNorm())
+	plt.colorbar()
+	plt.yscale('log')
+	plt.xlabel('Sum RMS')
+	plt.ylabel('FLast')
+	plt.savefig('2d')
+	
+	fig2 = plt.figure()
+	plt.hist(c,100,histtype='step',label='e')
+	plt.hist(d,100,histtype='step',label='p')
+	plt.xlabel('Sum RMS')
+	plt.legend(loc='best')
+	plt.savefig('sumRMS')
+	
+	fig3 = plt.figure()
+	plt.hist2d(c,a,bins=80,norm=matplotlib.colors.LogNorm())
+	plt.colorbar()
+	plt.yscale('log')
+	plt.xlabel('Sum RMS')
+	plt.ylabel('FLast')
+	plt.savefig('2d_electrons')
+	
+	fig4 = plt.figure()
+	plt.hist2d(d,b,bins=80,norm=matplotlib.colors.LogNorm())
+	plt.colorbar()
+	plt.yscale('log')
+	plt.xlabel('Sum RMS')
+	plt.ylabel('FLast')
+	plt.savefig('2d_protons')
 	
 
 if __name__ == '__main__':
 	
-	xe, xp = run()
+	xe, xp, fe, fp, re, rp = run()
 	
 	makeHisto(xe,xp)
+	
+	make2DHisto(fe,fp,re,rp)
