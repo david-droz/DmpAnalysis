937d0cb0ead6425e0b7c753af3a39da3071b6015
David Francois Droz <david.droz@outlook.com>
2017-09-05 15:16:37 +0200

    Adapted to new data, now makes both normalised and non-normalised histograms

--- makeHistograms.py
+++ makeHistograms.py
@@ -16,6 +16,9 @@ from matplotlib.backends.backend_pdf import PdfPages
 from scipy import stats
 
 def getLabels():
+	'''
+	Names of all the variables
+	'''
 	
 	lab = []
 	
@@ -25,24 +28,28 @@ def getLabels():
 	erms = 'BGO_E_RMS_layer_'
 	for i in range(14):
 		lab.append(erms + str(i))
+	ehit = 'BGO_E_HITS_layer_'
+	for i in range(14):
+		lab.append(ehit + str(i))
 	lab.append('BGO_RMS_longitudinal')
 	lab.append('BGO_RMS_radial')
 	lab.append('BGO_E_total_corrected')
 	lab.append('BGO_total_hits')
+	lab.append('BGO_theta_angle')
 	
 	for i in range(2):
 		lab.append('PSD_E_layer_' + str(i))
 	for i in range(2):
 		lab.append('PSD_hits_layer_' + str(i))
-	for k in ['1a','1b','2a','2b']:
-		lab.append('PSD_E_RMS_layer_' + k)
 		
 	lab.append('STK_NClusters')
 	lab.append('STK_NTracks')
-	for i in range(8):
+	for i in range(4):
 		lab.append('STK_E_' + str(i))
-	for i in range(8):
+	for i in range(4):
 		lab.append('STK_E_RMS_' + str(i))
+	for i in range(4):
+		lab.append('NUD_channel_'+str(i))
 		
 	lab.append('timestamp')
 	lab.append('label')
@@ -62,27 +69,32 @@ if __name__ == '__main__':
 	arr_elecs = np.load(sys.argv[1])[:,0:-2]
 	arr_prots = np.load(sys.argv[2])[:,0:-2]
 	
-	arr_elecs = arr_elecs / arr_elecs.max(axis=0)
-	arr_prots = arr_prots / arr_prots.max(axis=0)
+	arr_elecs_n = arr_elecs / arr_elecs.max(axis=0)
+	arr_prots_n = arr_prots / arr_prots.max(axis=0)
 		
 	lab = getLabels()
 	
-	pp = PdfPages('multipage.pdf')
+	pp_n = PdfPages('allVars_norm.pdf')
+	pp = PdfPages('allVars.pdf')
 	
 	for i in range(arr_elecs.shape[1]):
-		
-		#~ KS_statistic, p_value = stats.ks_2samp(arr_elecs[:,i],arr_prots[:,i])	# Kolmogorov-Smirnov test
-																				# If p-value is high, then the two distributions are likely the same
-																				# If K-S statistic is high, then the two distributions are likely different.
 		
 		f1 = plt.figure()
 		plt.hist(arr_elecs[:,i], 50, normed=1,histtype='step', label='e')
 		plt.hist(arr_prots[:,i], 50, normed=1,histtype='step', label='p')
 		plt.legend(loc='best')
-		#~ plt.xscale("log")
 		plt.yscale('log')
-		#~ plt.title(lab[i] + '\n K-S : ' + str(KS_statistic))
 		plt.title(lab[i])
 		plt.savefig(pp, format='pdf')
 		plt.close(f1)
+		
+		f2 = plt.figure()
+		plt.hist(arr_elecs_n[:,i], 50, normed=1,histtype='step', label='e')
+		plt.hist(arr_prots_n[:,i], 50, normed=1,histtype='step', label='p')
+		plt.legend(loc='best')
+		plt.yscale('log')
+		plt.title(lab[i])
+		plt.savefig(pp_n, format='pdf')
+		plt.close(f2)
+		
 	pp.close()
