e106ad7daa77095915c27ccffda3d4fc1905971d
David Francois Droz <david.droz@outlook.com>
2017-11-20 18:19:42 +0100

    More documentation

--- cppWrapper/compile.sh
+++ cppWrapper/compile.sh
@@ -6,5 +6,5 @@ g++ \
  -I/cvmfs/dampe.cern.ch/rhel6-64/opt/externals/root/latest/include \
  -L/cvmfs/dampe.cern.ch/rhel6-64/opt/releases/latest/lib \
  -l DmpEvent \
- -o classifier Classifier.cpp \
- -fno-strict-aliasing -g -O2 -DNDEBUG -g -fwrapv -O3 -Wall -lpython2.7 testWrapper.cpp
+ -o example_DmpEvent \
+ -fno-strict-aliasing -g -O2 -DNDEBUG -g -fwrapv -O3 -Wall -lpython2.7 Classifier.cpp example_DmpEvent.cpp
--- cppWrapper/getFiles.sh
+++ cppWrapper/getFiles.sh
@@ -5,4 +5,6 @@ wget -nv -N -O trainedDNN.h5 https://github.com/david-droz/DmpAnalysis/blob/mast
 wget -nv -N -O calculateScore.py https://raw.githubusercontent.com/david-droz/DmpAnalysis/master/cppWrapper/calculateScore.py
 wget -nv -N -O Classifier.cpp https://raw.githubusercontent.com/david-droz/DmpAnalysis/master/cppWrapper/Classifier.cpp
 wget -nv -N -O Classifier.h https://raw.githubusercontent.com/david-droz/DmpAnalysis/master/cppWrapper/Classifier.h
-wget -nv -N -O testWrapper.cpp https://raw.githubusercontent.com/david-droz/DmpAnalysis/master/cppWrapper/testWrapper.cpp
+wget -nv -N -O example_DmpEvent.cpp https://raw.githubusercontent.com/david-droz/DmpAnalysis/master/cppWrapper/example_DmpEvent.cpp
+wget -nv -N -O example_ROOT.cpp https://raw.githubusercontent.com/david-droz/DmpAnalysis/master/cppWrapper/example_ROOT.cpp
+wget -nv -N -O compile.sh https://raw.githubusercontent.com/david-droz/DmpAnalysis/master/cppWrapper/compile.sh
