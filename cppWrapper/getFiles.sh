95f0ad889cf7e128677f09512e08f74b1691cba9
David Francois Droz <david.droz@outlook.com>
2017-11-20 16:24:23 +0100

    Changed download options

--- cppWrapper/getFiles.sh
+++ cppWrapper/getFiles.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget -nv -O X_max.npy https://github.com/david-droz/DmpAnalysis/blob/master/cppWrapper/X_max.npy?raw=true
-wget -nv -O trainedDNN.h5 https://github.com/david-droz/DmpAnalysis/blob/master/cppWrapper/trainedDNN.h5?raw=true
-wget -nv https://raw.githubusercontent.com/david-droz/DmpAnalysis/master/cppWrapper/calculateScore.py
-wget -nv https://raw.githubusercontent.com/david-droz/DmpAnalysis/master/cppWrapper/Classifier.cpp
-wget -nv https://raw.githubusercontent.com/david-droz/DmpAnalysis/master/cppWrapper/Classifier.h
-wget -nv https://raw.githubusercontent.com/david-droz/DmpAnalysis/master/cppWrapper/testWrapper.cpp
+wget -nv -N -O X_max.npy https://github.com/david-droz/DmpAnalysis/blob/master/cppWrapper/X_max.npy?raw=true
+wget -nv -N -O trainedDNN.h5 https://github.com/david-droz/DmpAnalysis/blob/master/cppWrapper/trainedDNN.h5?raw=true
+wget -nv -N -O calculateScore.py https://raw.githubusercontent.com/david-droz/DmpAnalysis/master/cppWrapper/calculateScore.py
+wget -nv -N -O Classifier.cpp https://raw.githubusercontent.com/david-droz/DmpAnalysis/master/cppWrapper/Classifier.cpp
+wget -nv -N -O Classifier.h https://raw.githubusercontent.com/david-droz/DmpAnalysis/master/cppWrapper/Classifier.h
+wget -nv -N -O testWrapper.cpp https://raw.githubusercontent.com/david-droz/DmpAnalysis/master/cppWrapper/testWrapper.cpp
