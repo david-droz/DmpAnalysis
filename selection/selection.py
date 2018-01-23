7a1b3da05ad8a144abccf0155b4883760dc92e70
David <david.droz@outlook.com>
2018-01-23 20:33:28 +0100

    Added the cuts in code instead of importing

--- selection/selection.py
+++ selection/selection.py
@@ -32,7 +32,74 @@ import gc
 gc.enable()
 import yaml
 
-from ../skim/MC_skimmer import containmentCut, cutMaxELayer, maxBarCut
+
+def containmentCut(bgorec):
+	'''
+	BGO Acceptance cut: top (Z=46) and bottom BGO (Z=448) projection within 280 mm from the center
+	'''
+	
+	BGO_TopZ = 46
+	BGO_BottomZ = 448
+	
+	bgoRec_slope = [  bgorec.GetSlopeYZ() , bgorec.GetSlopeXZ() ]
+	bgoRec_intercept = [ bgorec.GetInterceptXZ() , bgorec.GetInterceptYZ() ]
+	
+	if (bgoRec_slope[1]==0 and bgoRec_intercept[1]==0) or (bgoRec_slope[0]==0 and bgoRec_intercept[0]==0): 
+		return False
+	
+	topX = bgoRec_slope[1]*BGO_TopZ + bgoRec_intercept[1]
+	topY = bgoRec_slope[0]*BGO_TopZ + bgoRec_intercept[0]
+	bottomX = bgoRec_slope[1]*BGO_BottomZ + bgoRec_intercept[1]
+	bottomY = bgoRec_slope[0]*BGO_BottomZ + bgoRec_intercept[0]
+	if not all( [ abs(x) < 280 for x in [topX,topY,bottomX,bottomY] ] ):
+		return False
+		
+	return True
+	
+	
+def cutMaxELayer(bgorec,cutValue=0.35):
+	'''
+	The maximum of the energy deposited in a single layer must be lower than 35% of the total energy
+	This cut removes side-entering events, that would deposit most of their energy in a single layer (since layers are horizontal)
+	cutValue=0.35 corresponds to the cut applied on flight data.
+	'''
+	
+	ELayer_max = 0
+	for i in range(14):
+		e = bgorec.GetELayer(i)
+		if e > ELayer_max: ELayer_max = e
+	
+	try:
+		rMaxELayerTotalE = ELayer_max / bgorec.GetTotalEnergy()
+	except ZeroDivisionError:
+		return False
+	if rMaxELayerTotalE > cutValue: 
+		return False
+		
+	return True
+	
+	
+def maxBarCut(event):
+	'''
+	The maximum of the energy deposited in the first 3 layers must not be on the side of the layer.
+	This cut removes events whose showers won't be well contained.
+	'''
+	
+	barNumberMaxEBarLay1_2_3 = [-1 for i in [1,2,3]]
+	MaxEBarLay1_2_3 = [0 for i in [1,2,3]]
+	for ihit in range(0, event.pEvtBgoHits().GetHittedBarNumber()):
+		hitE = (event.pEvtBgoHits().fEnergy)[ihit]
+		lay = (event.pEvtBgoHits().GetLayerID)(ihit)
+		if lay in [1,2,3]:
+			if hitE > MaxEBarLay1_2_3[lay-1]:
+				iBar =  ((event.pEvtBgoHits().fGlobalBarID)[ihit]>>6) & 0x1f		# What the fuck?
+				MaxEBarLay1_2_3[lay-1] = hitE
+				barNumberMaxEBarLay1_2_3[lay-1] = iBar
+	for j in range(3):
+		if barNumberMaxEBarLay1_2_3[j] <=0 or barNumberMaxEBarLay1_2_3[j] == 21:
+			return False
+					
+	return True
 
 def openRootFile(efilelist): 
 	'''
